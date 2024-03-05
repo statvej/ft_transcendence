479e43698b634ff9f358d658cbd39f151ec2308c
Fedir Staryk <statvej@gmail.com>
2024-03-05 16:14:42 +0100

    feat: check db url in app

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,12 +4,12 @@ import { ValidationPipe } from "@nestjs/common";
 import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
+  console.log(process.env.DB_URL);
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({whitelist: true}));
   app.use(cookieParser());
   app.enableCors({origin:"*", credentials: true});
   await app.listen(5500, '0.0.0.0', () => {
-    console.log(process.env.DB_URL);
     console.log('NestJS application is listening on port 5500');
   });
 }
